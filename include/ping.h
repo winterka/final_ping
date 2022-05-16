c435e10f90d5eb824ee0f04781748fa4e16996a3
Oleg <74597383+winterka@users.noreply.github.com>
2022-05-17 01:56:20 +0300

    idk wtf im doin' :))))))

--- include/ping.h
+++ include/ping.h
@@ -20,42 +20,46 @@
 
 class Ping {
 private:
+    //переменные для хранения адреса
     std::string input_domain;       
     std::string backup_ip;          
-
+    //дескриптор сокета
     int sock_fd;
-
+    //максимальное время ожидания пакета
     int max_wait_time;              
-
+    //переменные для статистики
     int send_pack_num;              
     int recv_pack_num;              
     int lost_pack_num;              
-
+    //адреса сокетов
     struct sockaddr_in send_addr;   
     struct sockaddr_in recv_addr;   
-
+    //размеры пакетов
     char send_pack[PACK_SIZE];      
     char recv_pack[PACK_SIZE + 20]; 
-
+    //время отправки и получения пакетов
     struct timeval first_send_time; 
     struct timeval recv_time;       
-
+    //переменные для статистики
     double min_time;
     double max_time;
     double sum_time;
 
-
+    //создание пакета
     int GeneratePacket();
+    //обработка пакета
     int ResolvePakcet(int pack_szie);
-
+    //расчет контрольной суммы
     unsigned short CalculateCksum(unsigned short * send_pack, int pack_size);
 
 public:
+    //функция инициализации основных параметров
     Ping(const char * ip, int max_wait_time);
+    //обработка закрытия сокета
     ~Ping();
-
+    //создание сокета
     void CreateSocket();
-
+    //отправка,поулучение пакетов и вывод статистики
     void SendPacket();
     void RecvPacket();
     void statistic();
--- include/utils.h
+++ include/utils.h
@@ -1,5 +1,6 @@
 #ifndef UTILS_H
 #define UTILS_H
 
-int checkParams(int argc, char *argv[]);
+int checkParams(int argc, char *argv[]); // Проверка входных параметров
+int pinging(int argc, char * argv[]); //Старт работы утилиты
 #endif
